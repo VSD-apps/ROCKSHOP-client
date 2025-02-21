e9f485c05c5adf87441e5f7e636a7958327e4926
wjs5025 <wjs_5025@naver.com>
2025-02-22 02:19:19 +0900

    chore: tailwind 설정 및 반응형 기초 레이아웃 설계 (#2)

--- src/app/_layouts/index.tsx
+++ src/app/_layouts/index.tsx
@@ -1,18 +1,6 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-
 import '../styles';
+import type { Metadata } from 'next';
 import { createClient } from '../lib/supabase/server';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -30,7 +18,10 @@ export async function RootLayout({
 
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
--- src/widgets/main-top-nav/ui/index.tsx
+++ src/widgets/main-top-nav/ui/index.tsx
@@ -5,11 +5,17 @@ import { SearchBtn } from './search-btn';
 
 export function MainTopNav() {
   return (
-    <header style={{ display: 'flex', gap: '12px' }}>
-      <Logo />
-      <Menu />
-      <SearchBtn />
-      <HamburgerBtn />
+    <header className="bg-black">
+      <div className="mx-auto flex justify-between items-center text-white w-full max-w-[600px] h-[72px] desktop:max-w-[1200px] desktop:h-[100px]">
+        <div className="flex gap-5">
+          <Logo />
+          <SearchBtn />
+        </div>
+        <div className="flex gap-5 hidden desktop:block">
+          <Menu />
+        </div>
+        <div className="flex gap-5 desktop:hidden">TODO : Mobile Menu</div>
+      </div>
     </header>
   );
 }
