b431f9c78203b26b948d85599bb886feec54d4f4
wjs5025 <wjs_5025@naver.com>
2025-02-24 00:21:41 +0900

    chore: 로고 컴포넌트 추가 (#2)

--- src/widgets/main-top-nav/ui/index.tsx
+++ src/widgets/main-top-nav/ui/index.tsx
@@ -1,5 +1,5 @@
+import Logo from '@/shared/ui/logo';
 import { HamburgerBtn } from './hanburger-btn';
-import { Logo } from './logo';
 import { Menu } from './menu';
 import { SearchBtn } from './search-btn';
 
@@ -8,7 +8,7 @@ export function MainTopNav() {
     <header className="bg-black">
       <div className="mx-auto flex justify-between items-center text-white w-full max-w-[600px] h-[72px] desktop:max-w-[1200px] desktop:h-[100px]">
         <div className="flex gap-5">
-          <Logo />
+          <Logo className="w-[96px] h-[24px] desktop:w-[136px] desktop:h-[34px]" />
           <SearchBtn />
         </div>
         <div className="flex gap-5 hidden desktop:block">
